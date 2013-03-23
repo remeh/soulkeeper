21be0084a63296897cf8dd4b62790cdac26c9fe1
youri gicquel <gicque_y@pc-gicque_y.(none)>
2013-03-23 21:46:49 +0100

    modif mineure du déplacement des indiens

--- indian.lua
+++ indian.lua
@@ -18,15 +18,16 @@ Indian = {
             local direction = math.random(4)
             local dx = 0
             local dy = 0
+	    local prec = 0
 
-            if direction == Direction.UP then
-                dy = -1    
-            elseif direction == Direction.RIGHT then
-                dx = 1 
-            elseif direction == Direction.DOWN then
-                dy = 1
-            else 
-                dx = -1
+            if direction == Direction.UP and prec ~= Direction.UP then
+                dy = -(math.random(100)%2)
+            elseif direction == Direction.RIGHT and prec ~= Direction.RIGHT then
+                dx = (math.random(100)%2)
+            elseif direction == Direction.DOWN and prec ~= Direction.DOWN then
+                dy = (math.random(100)%2)
+            elseif prec ~= Direction.LEFT then 
+                dx = -(math.random(100)%2)
             end
 
             if not game.level:isBlocking(self.posX+dx, self.posY+dy) and not game.level:isOutside(self.posX + dx, self.posY + dy) then
