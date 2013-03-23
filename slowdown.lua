4d98f37fa6ba0c8ae37e3131d6b37ee87e8ee70c
Yenox <yenox@KDE-Pavillon.host>
2013-03-23 12:51:37 +0100

    separation des pieges en fichiers, ajout du new

---
+++ slowdown.lua
@@ -0,0 +1,10 @@
+Slowdown = {
+    class = "slowdown"
+    strength = 1,
+    soulNeeded = 2,
+    absorb_Area = 4,
+    effect_Area = 1,
+    cooldown = 1,
+    walk_on = false
+
+}
--- trap.lua
+++ trap.lua
@@ -1,50 +1,30 @@
-Trappe = {
-    type = "trappe",
-    strength = 100,
-    soulNeeded = 3,
-    pos_x = -1,
-    pos_y = -1,
-    Absorb_Area = 4,
-    effect_Area = 1,
-    couldown = 2,
-    walk_on = true
-}
-
-Totem = {
-    type = "totem",
+Piege = {
+    class = "piege"
     strength = 0,
-    soulNeeded = 3,
+    soulNeeded = 0,
     pos_x = -1,
     pos_y = -1,
-    Absorb_Area = 4,
-    effect_Area = 4,
-    couldown = 2,
+    absorb_Area = 0,
+    effect_Area = 0,
+    cooldown = 0,
     walk_on = false
+
 }
 
-Slow_Down = {
-    type = "slow_down",
-    strength = 0,
-    soulNeeded = 3,
-    pos_x = -1,
-    pos_y = -1,
-    Absorb_Area = 4,
-    effect_Area = 4,
-    couldown = 2,
-    walk_on = true
-}
-
-Repulse = {
-    type = "repulse",
-    strength = 0,
-    soulNeeded = 3,
-    pos_x = -1,
-    pos_y = -1,
-    Absorb_Area = 4,
-    effect_Area = 2,
-    couldown = 2,
-    walk_on = true
-}
+function Trappe:new(trappeType)
+	trappe = Piege
+	
+	if trappeType.class then
+		trappe.class = trappeType.class
+		trappe.strength = trappeType.strength
+		trappe.soulNeeded = trappeType.soulNeeded
+		trappe.absord_Area = trappeType.absorb_Area
+		trappe.effect_Area = trappeType.effect_Area
+		trappe.cooldown = trappeType.cooldown
+		trappe.walk_on = trappeType.walk_on
+	end
+	return trappe
+end
 
 function Trappe:contains(object)
    local x, y
