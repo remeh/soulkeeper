987e1da69b04383a8266523609a8013dfc0f71cf
Yenox <gege.dert@gmail.com>
2013-03-24 14:35:02 +0100

    cage

---
+++ cage.lua
@@ -0,0 +1,28 @@
+Cage = {
+	class = "cage",
+	strength = 100,
+	soulNeeded = 1,
+	area = 1,
+	cooldown = 2,
+	walk_on = true,
+	actorCaged = nil,
+
+	action = function(self, actor)
+		if  self.class ~= "cage_full" then
+			actor.timeCaged = 33 
+			actor.isBlocked = true
+			actor.cage = self
+			self.actorCaged = actor
+			self.class = "cage_full"
+		else
+			if self.actorCaged ~= actor then
+				self.class = "cage"
+				self.actorCaged.timeCaged = -1
+				self.actorCaged.isBlocked = false
+				game.level:removeTrap(self)
+			end
+		end
+	end
+
+
+}
