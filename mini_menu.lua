35a58753206dc45da8c34dc9c6f7aeda34735aed
Emeric <mipcaram@hotmail.fr>
2013-03-24 11:21:44 +0100

    Size minimenu

--- mini_menu.lua
+++ mini_menu.lua
@@ -7,6 +7,7 @@ Minimenu = {
 		{"Totem", 545, 55, 630, 80, {230, 230, 230}, function(self) self:trap2() end,1},
 		{"Smoke", 545, 98, 630, 130, {230, 230, 230}, function(self) self:trap3() end,1},
 		{"Cage", 545, 136, 630, 160, {230, 230, 230}, function(self) self:trap4() end,1},
+		{"Kill", 545, 175, 630, 190, {230, 230, 230}, function(self) self:trap5() end,1},
 		{"    Pause", 510, 300, 615, 320, {230, 230, 230}, function(self) self:pause() end},
 		{"Tourists", 510, 272, 615, 280, {230, 230, 230}}
 	},
@@ -36,6 +37,10 @@ function Minimenu:trap4()
 	 print("Cage")
 end
 
+function Minimenu:trap5()
+	 print("Kill")
+end
+
 function Minimenu:pause()
 	game:switchPause()
 end
@@ -53,7 +58,7 @@ function Minimenu:draw()
 		end
 	end
 	love.graphics.print("Nxt wave: " .. game.gameplay:timeToNextWave(), 506, 243)
-	love.graphics.print(game.wave, 590, 272)
+	love.graphics.print(game.wave, 583, 272)
 	love.graphics.print(math.floor(game.point), 510, 328)
 	love.graphics.setColor(255,255,255)
 	love.graphics.draw(self.ghostImage,623,5)
